e7b33676dce0432c07c551088978452eb303a9dd
geprojetos <geprojetosbrasil@gmail.com>
2022-11-06 16:30:46 -0300

    refactor test movie repository

--- src/adapters-layer/repository/movie/create/index.spec.ts
+++ src/adapters-layer/repository/movie/create/index.spec.ts
@@ -1,41 +1,28 @@
 import InMemoryMovieRepository from "../../../../__test__/repository/movie/InMemoryMovieRepository";
-import { Stream } from "../../../../enterprise-layer/domain";
+import { IStream, Stream } from "../../../../enterprise-layer/domain";
 import Status from "../../../../utils/status";
 import Messages from "../../../../utils/messages";
 
 describe("MovieRepository", async () => {
+  const movieTest: IStream = {
+    title: "test",
+    category: "test",
+    description: "test",
+  };
   let inMemoryMovieRepository: InMemoryMovieRepository;
 
   beforeAll(() => {
     inMemoryMovieRepository = new InMemoryMovieRepository();
   });
 
-  test("should be able create movie", async () => {
-    const result = await inMemoryMovieRepository.create(
-      new Stream({
-        title: "test",
-        category: "test",
-        description: "test",
-      })
-    );
+  test("should be able create movie with status code 200", async () => {
+    const result = await inMemoryMovieRepository.create(new Stream(movieTest));
     expect(result.statusCode).toBe(200);
   });
 
-  test("should be able error create movie equal title", async () => {
-    await inMemoryMovieRepository.create(
-      new Stream({
-        title: "test",
-        category: "test",
-        description: "test",
-      })
-    );
-    const result = await inMemoryMovieRepository.create(
-      new Stream({
-        title: "test",
-        category: "test",
-        description: "test",
-      })
-    );
+  test("should be able error create movie with status code 409", async () => {
+    await inMemoryMovieRepository.create(new Stream(movieTest));
+    const result = await inMemoryMovieRepository.create(new Stream(movieTest));
     expect(result).toStrictEqual({
       statusCode: Status.code().conflict,
       message: Messages.movie().alreadyExisting,
