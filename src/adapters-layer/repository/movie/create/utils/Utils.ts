148e04b54cc9b7e53ec661da1d81a1f92c54be2e
geprojetos <geprojetosbrasil@gmail.com>
2023-01-12 11:31:10 -0300

    refactor(create movie): refactor create

--- src/adapters-layer/repository/movie/create/utils/Utils.ts
+++ src/adapters-layer/repository/movie/create/utils/Utils.ts
@@ -1,4 +1,4 @@
-import { Stream } from "../../../../../enterprise-layer/domain";
+import { IStream, Stream } from "../../../../../enterprise-layer/domain";
 import File from "../../../../utils/file";
 import { logger } from "../../../../utils/logger";
 import Messages from "../../../../utils/messages";
@@ -12,9 +12,7 @@ class Utils {
     this._file = file;
   }
 
-  public isInValid(props: ICreateMovieProps) {
-    const { movie } = props;
-
+  public isInValid(movie: Stream) {
     if (this._isValue(movie)) {
       logger.error(`${Messages.movie().invalidData}`);
       return {
@@ -32,8 +30,8 @@ class Utils {
     );
   }
 
-  public isAlreadyExisting(props: ICreateMovieProps) {
-    const { movie, contentFile } = props;
+  public async isAlreadyExisting(movie: Stream) {
+    const contentFile: IStream[] = await this._file.read();
     const isAlready = this._isDuplicated({
       contentFile,
       movie,
@@ -57,8 +55,8 @@ class Utils {
     );
   }
 
-  public async isSuccess(props: ICreateMovieProps) {
-    const { movie, contentFile } = props;
+  public async isSuccess(movie: Stream) {
+    const contentFile: IStream[] = await this._file.read();
     const isDuplicated = this._isDuplicated({
       contentFile,
       movie,
@@ -66,10 +64,8 @@ class Utils {
 
     if (!isDuplicated?.length) {
       contentFile?.push(movie.stream());
-      logger.info(
-        `${Messages.movie().saveInDataBase} => ${props.movie.stream().id}`
-      );
-      const response = await this._file.write(props?.contentFile || []);
+      logger.info(`${Messages.movie().saveInDataBase} => ${movie.stream().id}`);
+      const response = await this._file.write(contentFile || []);
       return {
         statusCode: response?.statusCode || Status.badRequest(),
         message: Messages.movie().saveInDataBase,
--- src/adapters-layer/repository/movie/create/utils/Validate.ts
+++ src/adapters-layer/repository/movie/create/utils/Validate.ts
@@ -1,4 +1,4 @@
-import { IStream, Stream } from "../../../../../enterprise-layer/domain";
+import { Stream } from "../../../../../enterprise-layer/domain";
 import { IConfig } from "../../../../utils/config";
 import File from "../../../../utils/file";
 import Utils from "./Utils";
@@ -13,20 +13,13 @@ class Validate {
   }
 
   public async isValidate(movie: Stream) {
-    const contentFile: IStream[] = await this._file.read();
-
-    const isInValid = this._utils.isInValid({
-      movie,
-    });
+    const isInValid = this._utils.isInValid(movie);
     if (isInValid) return isInValid;
 
-    const isAlreadyExisting = this._utils.isAlreadyExisting({
-      movie,
-      contentFile,
-    });
+    const isAlreadyExisting = await this._utils.isAlreadyExisting(movie);
     if (isAlreadyExisting) return isAlreadyExisting;
 
-    return this._utils.isSuccess({ movie, contentFile });
+    return await this._utils.isSuccess(movie);
   }
 }
 
