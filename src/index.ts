b0580653435fb3f3c0162ebceb5769ad48c39c63
geprojetos <geprojetosbrasil@gmail.com>
2023-01-13 12:19:28 -0300

    feat(documenttation): add documentation create movie

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import app from "./frameworks-layer/app";
 import { logger } from "./adapters-layer/utils/logger";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./frameworks-layer/swagger/swagger.json";
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3333, () => {
   logger.info("server listen port 3333");
